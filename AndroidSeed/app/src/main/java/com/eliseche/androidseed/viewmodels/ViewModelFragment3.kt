14ee00008c301a20ff0620e0bf727288985ca4d1
Alejandro Eliseche <eliseche@gmail.com>
2019-09-05 17:35:43 -0300

    Added some logic to Fragment3 (#1)

--- AndroidSeed/app/src/main/java/com/eliseche/androidseed/viewmodels/ViewModelFragment3.kt
+++ AndroidSeed/app/src/main/java/com/eliseche/androidseed/viewmodels/ViewModelFragment3.kt
@@ -1,8 +1,18 @@
 package com.eliseche.androidseed.viewmodels
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 class ViewModelFragment3 : ViewModel() {
+    val isVisible = MutableLiveData<Boolean>()
+    val someText = MutableLiveData<String>()
+
+    init {
+        isVisible.value = true
+        someText.value = "Hi, this is HTML with <b>bold</b> and with this <font color='#ff0000'>color</font>. " +
+                "We are using Html.from() in .xml with Data binding."
+    }
+
     fun init() {
     }
 }
--- AndroidSeed/app/src/main/java/com/eliseche/androidseed/views/interfaces/IViewModelBase.kt
+++ AndroidSeed/app/src/main/java/com/eliseche/androidseed/views/interfaces/IViewModelBase.kt
@@ -1,5 +1,12 @@
 package com.eliseche.androidseed.views.interfaces
 
+/**
+ * Used for binding every list item to a ViewModel instance of type item.
+ * For simple items without manipulation the idea is to bind every item
+ * with it's model. But when we need to do some data manipulation we bind with
+ * a ViewModel.
+ */
 interface IViewModelBase {
+    // Set item of type Any to the ViewModel
     fun setItem(item: Any?)
 }
